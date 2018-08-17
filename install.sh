ce7a00dc8b5ec87b914c2472d8fa19a20445f7be
zhongjun2 <jun.zhongjun@huawei.com>
2018-08-17 11:43:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -75,10 +75,7 @@ downloadTerraform(){
   else
   	echo "Started to download the terraform package"
   	checkCmdSuccess wget -O terraform_0.10.7_linux_amd64 https://releases.hashicorp.com/terraform/0.10.7/terraform_0.10.7_linux_amd64.zip
-    unzip
-	if [ ! $? -eq 0 ];then
-	  echo yes | apt install unzip
-	fi
+	echo yes | apt install unzip
 	unzip terraform_0.10.7_linux_amd64
   	echo "SUCCESS: Install terraform"
   fi
